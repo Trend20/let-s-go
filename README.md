67030eb7a971c507ecfe2ca6185d3a104fed3779
Trend20 <enockomondi305@gmail.com>
2024-09-25 00:59:20 +0300

    project updates

---
+++ README.md
@@ -0,0 +1,9 @@
+## Let's Go
+
+### Path Types
+
+**Fixed path:** has no trailing slash
+
+**Subtree path:** has a trailing slash(/)
+
+To make a check for the paths, just include a check of the URL path inside the handle function.
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-//handlers
+//define route handlers
+
+// home handler
+func home(w http.ResponseWriter, r *http.Request) {
+	//assertains the path
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Let's Go!"))
+}
+
+// snippet handler
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is your code snippet!"))
+}
+
+// create snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create your code snippet!"))
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	//initialize a server
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	//	listen to port
+	log.Println("Listening on port :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
